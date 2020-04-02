876ae4c6c50c51dbd307c8358904e124fc9466a3
Niha8030 <53756642+Niha8030@users.noreply.github.com>
2020-04-02 17:32:12 +0530

    Update AIRLINE.sql

--- AIRLINE.sql
+++ AIRLINE.sql
@@ -37,17 +37,17 @@ commit;
 insert into aircraft values(&aid,'&aname',&cruisingrange);
 insert into employess values(&eid,'&ename',&salary);
 insert into certified values(&eid,&aid);
-sql queries
-1.select distinct aname from aircrafts where aid in(select c.aid from certified c, employess e where c.eid=e.eid and not exists( select * from employess e1 where e1.eid=e.eid and e1.salary<80000));
-2.select c.eid ,max(a.cruisingrange) from certified c, aircrafts a where c.aid=a.aid group by c.eid having count(*)>3;
-3.select ename from employess
-where salary<(select min(f.price) from
-flights f where f.fromfl='bangalore' and f.tofl='frankfurt');
-4.select a.aname, e.salary from aircrafts a,employess e, certified c where c.aid=a.aid and c.eid=e.eid and a.cruisingrange>1000 group by a.aid,a.aname; 
-5.select distinct e.ename from employess e, certified c, aircrafts a where e.eid=c.eid and c.aid=a.aid and a.aname like 'boeing%';
-6.select aid from aircrafts a
-where a.cruisingrange>(select min(f.distance) from
-flights f where f.fromfl='bengaluru' and f.tofl='delhi');
-8.select e.ename, e.salary from employess e where e.eid not in ( select distinct c.eid from certified c ) and e.salary >( select avg (e1.salary)
-from employess e1 where e1.eid in ( select distinct c1.eid from certified c1 ) );
+
+1. select distinct aname from aircrafts where aid in(select c.aid from certified c, employess e where c.eid=e.eid and not exists( select * from employess e1 where e1.eid=e.eid and e1.salary<80000));
+2. select c.eid ,max(a.cruisingrange) from certified c, aircrafts a where c.aid=a.aid group by c.eid having count(*)>3;
+3. select ename from employess
+   where salary<(select min(f.price) from
+   flights f where f.fromfl='bangalore' and f.tofl='frankfurt');
+4. select a.aname, e.salary from aircrafts a,employess e, certified c where c.aid=a.aid and c.eid=e.eid and a.cruisingrange>1000 group by a.aid,a.aname; 
+5. select distinct e.ename from employess e, certified c, aircrafts a where e.eid=c.eid and c.aid=a.aid and a.aname like 'boeing%';
+6. select aid from aircrafts a
+   where a.cruisingrange>(select min(f.distance) from
+   flights f where f.fromfl='bengaluru' and f.tofl='delhi');
+8. select e.ename, e.salary from employess e where e.eid not in ( select distinct c.eid from certified c ) and e.salary >( select avg (e1.salary)
+   from employess e1 where e1.eid in ( select distinct c1.eid from certified c1 ) );
     
